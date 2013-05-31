502bac4ed5c3c388aa6e77e9db7f121563eb94b5
sembre <agusf8@gmail.com>
2013-06-01 01:43:13 +0700

    first commit

---
+++ common/include/generated/compile.h
@@ -0,0 +1,9 @@
+/* This file is auto generated, version 3 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#3 PREEMPT Thu May 9 09:14:32 WIT 2013"
+#define LINUX_COMPILE_TIME "09:14:32"
+#define LINUX_COMPILE_BY "sembre"
+#define LINUX_COMPILE_HOST "sembre"
+#define LINUX_COMPILE_DOMAIN "(none)"
+#define LINUX_COMPILER "gcc version 4.4.1 (Sourcery G++ Lite 2009q3-68) "
